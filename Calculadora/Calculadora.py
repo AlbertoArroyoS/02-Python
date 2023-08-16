8ab0bf64e7a701f17ef1580bdc09c9740560c453
Alberto Arroyo <albertoarroyo@hotmail.es>
2023-08-16 13:18:42 +0200

    Continuacion calculadora Tkinter

--- Calculadora/Calculadora.py
+++ Calculadora/Calculadora.py
@@ -14,7 +14,7 @@ coma=False
 
 #Variable para ir acumulando
 
-resultado=1
+resultado=0
 
 #Al pulsar se almacene en digitoDisplay y aparezca en el Entry creando una funcion 
 
@@ -23,12 +23,122 @@ digito_display=StringVar()
 
 #digito_display.set("0")
 
-display = Entry(mi_frame, textvariable=digito_display)
+display = Entry(mi_frame, textvariable=digito_display, font="Arial 15")
 
 
 display.grid(row=1, column=1, columnspan=4, pady=10)
 
-display.config(background="black", fg="green", justify="right", width=30)
+display.config(background="black", fg="green", justify="right", width=15)
+
+#Para que vea si hay algun numero antes pulsado, lo guarde y muestre el siguiente
+
+def pulsar_teclas(num_pulsado):
+    #Para que despues de pulsar una tecla de operacion no se acumules, solo los numeros
+
+    global operacion
+    
+
+    if operacion != "":
+
+        digito_display.set(num_pulsado)
+        # Hay que resetear operacion de nuevo para que concatene
+        operacion=""
+
+    else:
+        #que solo se pueda pulsar una vez al 0
+        if num_pulsado=="0" and digito_display.get()=="0":
+            digito_display.set("0")
+
+        #Que mantenga el 0 si hemos puesto una coma para los numero decimales
+        elif num_pulsado!="." and digito_display.get()=="0":
+            digito_display.set(digito_display.get() + num_pulsado)
+
+        #solucionar que no salga el cero delante 025
+        elif num_pulsado!="0" and digito_display.get()=="0":
+            digito_display.set(num_pulsado)
+
+        
+        else:
+            digito_display.set(digito_display.get() + num_pulsado)
+
+
+#--------Agregar funcionalidad a las operaciones
+
+
+# funcion suma
+# num va a ser lo que hay escrito en pantalla, lo que hay en el entry, lo que hay en el entry se considera texto
+
+def suma(num):
+
+    global operacion
+
+    global resultado
+    
+    resultado+=float(num)
+
+    operacion ="suma"
+
+    
+    if resultado.is_integer():
+        digito_display.set(int(resultado))
+        operacion ="suma"
+    else:
+        operacion ="suma"
+        digito_display.set(resultado)
+
+# funcion resta
+def resta(num):
+
+    global operacion
+
+    global resultado
+    
+    resultado-=float(num)
+
+    operacion ="resta"
+'''
+    if resultado.is_integer():
+        digito_display.set(int(resultado))
+        operacion ="resta"
+    else:
+        operacion ="resta"
+        digito_display.set(resultado)
+    '''
+# funcion =
+
+def total():
+
+    global resultado
+    
+    #Que haga la operacion con lo que habia antes de pulsar la operacion y lo que se pulso despues
+
+    #digito_display.set(resultado + int(digito_display.get()))
+
+    if(resultado+float(digito_display.get())).is_integer():
+        digito_display.set(int(resultado+float(digito_display.get())))
+        resultado=0
+    else:
+
+        digito_display.set(resultado+float(digito_display.get()))
+
+        resultado=0
+
+# unción solo para procesar la coma y con un for recorrer la pantalla para recorrer la pantalla si encuentra una coma
+
+def pulsacion_coma():
+
+    contador=0
+
+    for i in digito_display.get():
+
+        if i==".":
+
+            contador+=1
+        
+    if contador==0:
+        digito_display.set(digito_display.get()+".")
+    
+
 
 #Uso funciones lambda para que no cargue automaticamente el numero 7 al cargar el programa
 
@@ -62,7 +172,7 @@ boton2=Button(mi_frame, text="2", width=4, command=lambda:pulsar_teclas("2"))
 boton2.grid(row=4, column=2)
 boton1=Button(mi_frame, text="1", width=4, command=lambda:pulsar_teclas("1"))
 boton1.grid(row=4, column=3)
-botonrest=Button(mi_frame, text="-", width=4)
+botonrest=Button(mi_frame, text="-", width=4, command= lambda:resta(digito_display.get()))
 botonrest.grid(row=4, column=4)
 
 #------------cuarta fila-----------------
@@ -73,87 +183,8 @@ botoncoma=Button(mi_frame, text=".", width=4, command=lambda:pulsacion_coma())
 botoncoma.grid(row=5, column=2)
 botonigual=Button(mi_frame, text="=", width=4, command= lambda:total())
 botonigual.grid(row=5, column=3)
-botonsum=Button(mi_frame, text="+", width=4, command= lambda:suma(resultado))
+botonsum=Button(mi_frame, text="+", width=4, command= lambda:suma(digito_display.get()))
 botonsum.grid(row=5, column=4)
-
-#Para que vea si hay algun numero antes pulsado, lo guarde y muestre el siguiente
-
-def pulsar_teclas(num_pulsado):
-    #Para que despues de pulsar una tecla de operacion no se acumules, solo los numeros
-
-    global operacion
-    
-
-    if operacion != "":
-
-        digito_display.set(num_pulsado)
-        # Hay que resetear operacion de nuevo para que concatene
-        operacion=""
-
-    else:
-        #que solo se pueda pulsar una vez al 0
-        if num_pulsado=="0" and digito_display.get()=="0":
-            digito_display.set("0")
-
-        #Que mantenga el 0 si hemos puesto una coma para los numero decimales
-        elif num_pulsado!="." and digito_display.get()=="0":
-            digito_display.set(digito_display.get() + num_pulsado)
-
-        #solucionar que no salga el cero delante 025
-        elif num_pulsado!="0" and digito_display.get()=="0":
-            digito_display.set(num_pulsado)
-        
-        else:
-            digito_display.set(digito_display.get() + num_pulsado)
-
-
-#--------Agregar funcionalidad a las operaciones
-
-
-# funcion suma
-# num va a ser lo que hay escrito en pantalla, lo que hay en el entry, lo que hay en el entry se considera texto
-
-def suma(num):
-
-    global operacion
-
-    global resultado
-    
-    resultado+=int(num)
-
-    operacion ="suma"
-
-    digito_display.set(resultado)
-
-    print(resultado)
-
-# funcion =
-
-def total():
-
-    global resultado
-    
-    #Que haga la operacion con lo que habia antes de pulsar la operacion y lo que se pulso despues
-
-    digito_display.set(resultado + int(digito_display.get()))
-
-    resultado=0
-
-# unción solo para procesar la coma y con un for recorrer la pantalla para recorrer la pantalla si encuentra una coma
-
-def pulsacion_coma():
-
-    contador=0
-
-    for i in digito_display.get():
-
-        if i==".":
-
-            contador+=1
-        
-        if contador==0:
-
-            digito_display.set(digito_display.get()+ ".")
 
 
 
