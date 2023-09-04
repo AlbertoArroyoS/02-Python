02161a997933b60103a8894cecaf2b975638712a
Alberto Arroyo <albertoarroyo@hotmail.es>
2023-09-04 18:52:42 +0200

    Crea una tabla si no existe

--- CRUD Tkinter/Conexiones.py
+++ CRUD Tkinter/Conexiones.py
@@ -1,18 +1,28 @@
 from tkinter import *
 from tkinter import messagebox
+from tkinter import simpledialog
 import sqlite3
 
 #Creo conexion con la BBDD
 
 def conectar_BBDD():
-    mi_conexion=sqlite3.connect("NegocioUsuarios")
+    nombre_BBDD = simpledialog.askstring("BBDD", "Indroduzca el nombre de la BBDD")
 
-    mi_cursor= mi_conexion.cursor()
+    mi_conexion = sqlite3.connect(nombre_BBDD)
+    mi_cursor = mi_conexion.cursor()
 
     try:
-        mi_cursor.execute()
-
-        messagebox.showinfo("BBDD","Conexion correcta a la BBDD")
-
-    except:
-        messagebox.showinfo("BBDD","No conectado a la BBDD")
+        mi_cursor.execute('''
+            CREATE TABLE datosusuarios (
+                ID INTEGER PRIMARY KEY AUTOINCREMENT,
+                NOMBRE VARCHAR(50),
+                APELLIDO VARCHAR(80),
+                PASSWORD VARCHAR(50),
+                DIRECCION VARCHAR(80),
+                COMENTARIOS VARCHAR(200)
+            )
+        ''')
+        mi_conexion.commit()
+        messagebox.showinfo("BBDD", "BBDD Creada correctamente")
+    except sqlite3.OperationalError:
+        messagebox.showinfo("BBDD", "La BBDD ya existe")
